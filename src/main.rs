34907def597ba97c4a093e8fbe187ba34d55d466
Huu <63862081+HollowHuu@users.noreply.github.com>
2023-06-07 14:37:50 +0200

    You can now login

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,38 @@
 use std::env;
 
 // Serenity
-use serenity::async_trait;
+use serenity::{
+    async_trait,
+};
 use serenity::prelude::*;
 use serenity::model::channel::Message;
 use serenity::framework::standard::macros::{command, group};
 use serenity::framework::standard::{StandardFramework, CommandResult};
 
-#[group]
-#[commands(ping, sqrt)]
-struct General;
-
+mod commands;
+use commands::{fun::*};
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {}
 
+#[group]
+#[prefixes("general")]
+#[commands(ping, sqrt)]
+struct General;
+
+#[group]
+#[prefixes("fun")]
+#[commands(cat, shiba)]
+struct Fun;
+
+
 #[tokio::main]
 async fn main() {
     let framework = StandardFramework::new()
         .configure(|c| c.prefix("=="))
-        .group(&GENERAL_GROUP);
+        .group(&GENERAL_GROUP)
+        .group(&FUN_GROUP);
     
     let token = env::var("token").expect("token");
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
@@ -45,11 +57,11 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
 
 #[command]
 async fn sqrt(ctx: &Context, msg: &Message) -> CommandResult {
-    // Get the square root of a number
-    print!("Message: {}", msg.content);
-    let number: f64 = msg.content.parse().unwrap();
+    let args = msg.content.split_whitespace().collect::<Vec<&str>>();
+    let number = args[1].parse::<f64>().unwrap();
 
-    // assert!(number.is_err()); 
+
+    assert!(number >= 0.0, "number must be positive"); 
     let sqrt = number.sqrt();
     msg.reply(ctx, format!("The square root of {} is {}", number, sqrt)).await?;
     
